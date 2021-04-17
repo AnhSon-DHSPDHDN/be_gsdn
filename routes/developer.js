9ad5fc5553052f2cd123629223c18506889cc518
Mai Văn Quang <maiquang1470@gmail.com>
2021-04-17 14:53:28 +0700

    update

--- routes/developer.js
+++ routes/developer.js
@@ -2,20 +2,76 @@ const express = require('express');
 const { Message } = require('../configs/message');
 const router = express.Router();
 const Developer = require('../models/Developer')
+const multer = require('multer')
+const fs = require('fs')
+const FileType = require('file-type');
+const { middlewareAuthorUser, middlewareAuthorAdmin } = require('../auth/authorization');
 
 router.put('/:id', async (req, res) => {
   try {
     const { id } = req.params
     const developer = await Developer.findById(id);
-    if (developer) {
+    const update = {
+      fullName: req.body.fullName,
+      description: req.body.description
+    }
+    if (req.body.avatar) {
+      update["avatar"] = req.body.avatar
+    }
+    const updateDev = await Developer.findOneAndUpdate({ _id: id }, {
+      $set: update
+     })
+    if (updateDev) {
       res.status(200).send({
-        developer: developer
+        developer: updateDev
       })
     } else throw new Error()
   } catch (error) {
     res.status(403).send({
       message: Message.LOI_SERVER,
       error
+    })
+  }
+})
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './images/avatar')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 1000000
+  }
+})
+
+router.post('/upload', middlewareAuthorAdmin, upload.single('avatar'), async (req, res) => {
+  try {
+    const filepath = FileType.fromFile(req.file.path)
+    if (!filepath) {
+      fs.unlink(`${filepath}`, err => {
+        if (err) throw new Error(err)
+      })
+      throw new Error()
+    }
+    if ((await filepath).ext !== 'jpg' || (await filepath).mime !== 'image/jpeg') {
+      fs.unlink(`${filepath}`, err => {
+        if (err) throw new Error(err)
+      })
+      throw new Error()
+    }
+    return res.json({
+      imageLink: `${process.env.BASE_BE}avatar/${req.file.filename}`
+    })
+  } catch (error) {
+    res.status(400).send({
+      message: Message.LOI_SERVER,
+      error: error
     })
   }
 })
--- routes/users.js
+++ routes/users.js
@@ -8,10 +8,10 @@ const router = express.Router();
 const Users = require('../models/Users');
 const bcript = require('bcryptjs')
 
-router.delete('/:_id', middlewareAuthorAdmin, async (req, res) => {
+router.delete('/', middlewareAuthorAdmin, async (req, res) => {
   try {
-    const { _id } = req.params;
-    const user = await Users.findByIdAndDelete(_id);
+    const { ids } = req.body;
+    const user = await Users.deleteMany({ _id: { $in: ids } });
     if (user) {
       return res.status(200).send(user)
     }
@@ -25,7 +25,11 @@ router.delete('/:_id', middlewareAuthorAdmin, async (req, res) => {
 
 router.get('/', middlewareAuthorAdmin, async (req, res) => {
   try {
-    const allUser = await Users.find()
+    const allUser = await Users.find({ username: { $ne: "admin" } })
+    allUser.map(u => {
+      u.toObject();
+      delete u.password
+    })
     return res.send(allUser)
   } catch (error) {
     return res.status(500).send({
@@ -34,11 +38,87 @@ router.get('/', middlewareAuthorAdmin, async (req, res) => {
   }
 })
 
+
+router.put('/:id', middlewareAuthorAdmin, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userEdit = await Users.findById(id);
+    if (!userEdit) return res.status(404).send({
+      message: Message.TAI_KHOAN_KHONG_TON_TAI
+    })
+    const userFound = await Users.findOne({ username: req.body.username, _id: { $ne: id } });
+    if (userFound) {
+      return res.status(402).send({
+        message: Message.TAI_KHOAN_TON_TAI
+      })
+    } else {
+      const update = {
+        username: req.body.username,
+        email: req.body.email,
+        _idRole: req.body._idRole,
+      }
+      if (req.body.password) {
+        update["password"] = await bcript.hash(req.body.password, 10)
+      }
+      const user = await Users.findOneAndUpdate({ _id: id }, {
+        $set: update
+      });
+      return res.status(200).send({
+        message: Message.CAP_NHAT_THANH_CONG,
+        user: user
+      })
+    }
+  } catch (error) {
+    return res.status(500).send({
+      message: Message.LOI_SERVER,
+      error: error
+    })
+  }
+})
+
+router.post('/create', middlewareAuthorAdmin, async (req, res) => {
+  try {
+    const userFound = await Users.findOne({ username: req.body.username });
+    if (userFound) {
+      return res.status(402).send({
+        message: Message.TAI_KHOAN_TON_TAI
+      })
+    } else {
+      const users = new Users({
+        _id: uuid(),
+        username: req.body.username,
+        password: await bcript.hash(req.body.password, 10),
+        email: req.body.email,
+        _idRole: req.body._idRole,
+        createBy: req.user.createBy
+      })
+      return users
+        .save()
+        .then(result => {
+          res.status(200).send({
+            message: Message.DANG_KY_THANH_CONG,
+            user: result
+          })
+        }).catch(error => {
+          res.status(500).send({
+            message: Message.LOI_SERVER,
+            error: error
+          })
+        })
+    }
+  } catch (error) {
+    return res.status(500).send({
+      message: Message.LOI_SERVER,
+      error: error
+    })
+  }
+})
+
 router.post('/', async (req, res) => {
   try {
     const userFound = await Users.findOne({ username: req.body.username });
     if (userFound) {
-      return res.status(200).send({
+      return res.status(402).send({
         message: Message.TAI_KHOAN_TON_TAI
       })
     } else {
